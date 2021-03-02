7b5bc8531af4504d7f9f0cec259c122c805ccad4
Aashutosh17 <Aashutoshpaneru17@icloud.com>
2021-03-02 11:00:50 +0545

    intial commit

---
+++ venv/PythonLab1/demo.py
@@ -0,0 +1,10 @@
+
+#Examples of debugging
+dstring=input("How far did you travel today (in miles)? ")
+tstring=input("How long did it take you (in hours)? ")
+dfloat= float(dstring)
+tfloat= float(tstring)
+s=dfloat/tfloat
+print("Your speed was" +str(s)+"miles per hour ")
+
+
---
+++ venv/PythonLab1/quesno.3.py
@@ -0,0 +1,7 @@
+#3 3. N students take k apples and distribute them among eachother evenly.
+# The remaining parts remain in the basket
+# How many apples will remain in the basket?The program read the numbers N and k.
+N=int(input("enter the number of students in class:"))
+A=int(input("enter the number of apples:"))
+t=A//N
+n=
