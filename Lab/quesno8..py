21bb7a544f2d5ff93bc548d3f962d250f98ed90d
Aashutosh17 <Aashutoshpaneru17@icloud.com>
2021-03-09 12:20:07 +0545

    initail commit

---
+++ Lab/quesno8..py
@@ -0,0 +1,6 @@
+# Write a python program which accepts the radius of a circle from the user and compute the area
+#(area of circle= =π r²)
+pi = 22/7
+r = int(input("Enter the value of radius"))
+area = pi*r**2
+print (area)
