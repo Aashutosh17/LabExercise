65a7dee054b102bf79b423d2f8cba78c2a62383e
Aashutosh17 <Aashutoshpaneru17@icloud.com>
2021-03-24 14:42:35 +0545

    Initial commit

---
+++ examques.py
@@ -0,0 +1,37 @@
+#the python code below calculates the multiplication of 3 up to 10.Rewrite the code using  for loop.
+i = 1
+num = 3
+for i in range (1,11):
+    prod=num*i
+    print (f'{num}*{i}={prod}')
+
+#What will be the output of following program?Rewrite the code using while loop to display same output.
+i=0
+while i<5:
+    if i==3:
+     break
+    print(i)
+    i=i+1
+
+#What will be the output of following program? Rewrite the code using for loop to display same output.
+
+for i in range (5):
+    i += 1
+    if i==3 :
+        continue
+    print(i)
+
+#The python code below generates 50 random even integers.Rewrite the code so it uses a while loop instead of for loop
+
+#import random
+#my_list =[]
+#for i in range (50):
+#    num = random.randint(1,1200)
+#   if num % 2 == 0:
+#        my_list.append(num)
+
+i=0
+mylist=[]
+while i<=:
+    i=i+1
+
--- guess the random number.py
+++ guess the random number.py
@@ -1,7 +1,3 @@
-import random
-num=random.randint(1,100)
-guess=int(input("please guess the number?"))
-while num !=guess:
-    guess=int(input('uffs !! try agian :guess again:'))
-else:
-    print('Congratulation!!! U r right')
+for i in range (4):
+for j in range (10):
+    print(*
