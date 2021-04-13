247e3811c3ebe76dbb82981eaa351acac19285a9
Aashutosh17 <Aashutoshpaneru17@icloud.com>
2021-04-13 11:10:15 +0545

    Initial commit

--- democlass.py
+++ democlass.py
@@ -1,10 +1,1 @@
 
-
-# multiplication table function
-def mul_table(num):
-    for i in range (1,11):
-        product=num*i
-        print(f'{num}*{i}={product}')
-
-num=int(input('Enter the value for multiplication'))
-mul_table(num)
